8700ec33b96cbec9624affb7e06305004be0e010
Alvarez Moran <cesar.alvarez-moran@capgemini.com>
2018-10-09 15:45:07 +0200

    modificaciones de eslint

--- app.js
+++ app.js
@@ -1,69 +1,61 @@
-//dependencias
+// dependencias
 const fetch = require('node-fetch');
-const jsdom = require("jsdom");
-var selectPortfolioSetup = require("./models/bogledb");
+const jsdom = require('jsdom');
+const selectPortfolioSetup = require('./models/bogledb');
+
 const jsdomObj = jsdom.JSDOM;
+const dbFilePath = './models/bogle.db';
+const query1 = 'select * from portfolioSetup';
 
-var dbFilePath = './models/bogle.db';
-var query1 = "select * from portfolioSetup";
-
-//construye url a partir del id
-var urlMorningStar = function (id) {
-    return "http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=" + id;
-};
+// construye url a partir del id
+function urlMorningStar(id) {
+  return `http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=${id}`;
+}
 
 async function main() {
-    var oPromise = new Array();
-    var oRetSelect = new Array();
+  const oPromise = [];
+  let oRetSelect = []];
 
-    //recupera entradas de BBDD portfoliosetup
-    await selectPortfolioSetup(dbFilePath, query1)
-        .then((retSelect) => oRetSelect = retSelect)
-        .catch((error) => console.log(error));
+  // recupera entradas de BBDD portfoliosetup
+  await selectPortfolioSetup(dbFilePath, query1)
+    .then(retSelect => (oRetSelect = retSelect))
+    .catch(error => console.log(error));
 
-    //informa array de promesas con las entradas anteriores
-    for (let i in oRetSelect) {
+  // informa array de promesas con las entradas anteriores
+  for (const i in oRetSelect) {
+    oPromise.push(fetch(urlMorningStar(oRetSelect[i].IDFONDO)));
+  }
 
-        oPromise.push(fetch(urlMorningStar(oRetSelect[i].IDFONDO)));
+  // scrapea los precios de los websites
+  const oPrices =
+    // recupera raw html
+    Promise.all(oPromise)
+      .then((res) => {
+        const oResProm = new Array();
+        for (const a in res) {
+          oResProm.push(res[a].text());
+        }
+        return Promise.all(oResProm);
+      })
+      // parsea html
+      .then((res) => {
+        const oParsea = function (text) {
+          // crea DOM y recupera campo
+          const dom = new jsdomObj(text);
+          const sVal = dom.window.document.getElementsByClassName('line text')[0].textContent;
+          // parsea precio
+          const sPrice = sVal.split(/\s/)[1].replace(',', '.');
+          const nPrice = parseFloat(sPrice);
+          return nPrice;
+        };
 
-    };
+        const oPrecios = res.map(oBody => oParsea(oBody));
+        return oPrecios;
+      })
+      .catch(console.log('error'));
 
-    //scrapea los precios de los websites
-    var oPrices =
-        //recupera raw html
-        Promise.all(oPromise).then(function (res) {
-            var oResProm = new Array();
-            for (let a in res) {
-                oResProm.push(res[a].text())
-            };
-            return Promise.all(oResProm);
-        }).
-            //parsea html
-            then(function (res) {
-                var oParsea = function (text) {
-                    // crea DOM y recupera campo
-                    const dom = new jsdomObj(text);
-                    var sVal = dom.window.document.
-                        getElementsByClassName("line text")[0].textContent
-                    // parsea precio
-                    var sPrice = sVal.split(/\s/)[1].replace(",", ".");
-                    var nPrice = parseFloat(sPrice);
-                    return nPrice;
-                };
-
-                var oPrecios =
-                    res.map(function (oBody) {
-                        return oParsea(oBody)
-                    });
-                return oPrecios;
-            }).
-            catch(console.log("error"));
-
-            
-    await oPrices.then(res => console.log(res));
-    console.log("fin");
-
-};
+  await oPrices.then(res => console.log(res));
+  console.log('fin');
+}
 
 main();
-
