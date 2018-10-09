021343706ed3e30a746c3d214b33369260cad610
Alvarez Moran <cesar.alvarez-moran@capgemini.com>
2018-10-09 16:26:08 +0200

    mas cambios

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const jsdom = require('jsdom');
 const selectPortfolioSetup = require('./models/bogledb');
 
-const jsdomObj = jsdom.JSDOM;
+const JsdomObj = jsdom.JSDOM;
 const dbFilePath = './models/bogle.db';
 const query1 = 'select * from portfolioSetup';
 
@@ -14,11 +14,14 @@ function urlMorningStar(id) {
 
 async function main() {
   const oPromise = [];
-  let oRetSelect = []];
+  let oRetSelect = [];
 
   // recupera entradas de BBDD portfoliosetup
   await selectPortfolioSetup(dbFilePath, query1)
-    .then(retSelect => (oRetSelect = retSelect))
+    // .then(retSelect => (oRetSelect = retSelect))
+    .then((retSelect) => {
+      oRetSelect = retSelect;
+    })
     .catch(error => console.log(error));
 
   // informa array de promesas con las entradas anteriores
@@ -27,32 +30,33 @@ async function main() {
   }
 
   // scrapea los precios de los websites
-  const oPrices =
-    // recupera raw html
-    Promise.all(oPromise)
-      .then((res) => {
-        const oResProm = new Array();
-        for (const a in res) {
-          oResProm.push(res[a].text());
-        }
-        return Promise.all(oResProm);
-      })
-      // parsea html
-      .then((res) => {
-        const oParsea = function (text) {
+  const oPrices = Promise.all(oPromise)
+    .then((res) => {
+      const oResProm = [];
+      for (const a in res) {
+        oResProm.push(res[a].text());
+      }
+      return Promise.all(oResProm);
+    })
+    // parsea html
+    .then(
+      (res) => {
+        function oParsea(text) {
           // crea DOM y recupera campo
-          const dom = new jsdomObj(text);
+          const dom = new JsdomObj(text);
           const sVal = dom.window.document.getElementsByClassName('line text')[0].textContent;
           // parsea precio
           const sPrice = sVal.split(/\s/)[1].replace(',', '.');
           const nPrice = parseFloat(sPrice);
           return nPrice;
-        };
+        }
 
         const oPrecios = res.map(oBody => oParsea(oBody));
         return oPrecios;
-      })
-      .catch(console.log('error'));
+      },
+      ( rej ) => { console.log(`error en promise.all ${rej}`) },
+    )
+    .catch(console.log('error'));
 
   await oPrices.then(res => console.log(res));
   console.log('fin');
